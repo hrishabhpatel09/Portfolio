4cec17d8f68aa75beb0974be4b4721adc6430f93
Hrishabh Patel <hrishabhpatel2023@gmail.com>
2024-05-23 17:09:32 +0530

    Created HomePage and designed HomePage Partially.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-
+import logo from '../../public/nodejs-icon.svg'
 function Navbar() {
   return (
-    <div className='h-12 shadow-md flex justify-around items-center'>
-      <div>logo</div>
-      <div className='hidden sm:flex'>
+    <div className='h-12 shadow-md flex justify-between items-center'>
+      <div className='ml-[2vw]'>
+        <img src={logo} alt='logo' className='w-8 h-8'/>
+      </div>
+      <div className='hidden sm:flex mr-72'>
         <ul className='flex gap-10'>
             <NavLink to={'/'}  className={({isActive})=>isActive?"text-blue-900 font-bold border-b-2 border-blue-900":""}><li>Home</li></NavLink>
             <NavLink to={'/about'}  className={({isActive})=>{return isActive?"text-blue-900 font-bold border-b-2 border-blue-900":""}}><li>About</li></NavLink>
@@ -15,8 +17,12 @@ function Navbar() {
             <NavLink to={'/contact'}  className={({isActive})=>{return isActive?"text-blue-900 font-bold border-b-2 border-blue-900":""}}><li>Contact</li></NavLink>
         </ul>
       </div>
-      <div className='flex flex-col items-center sm:hidden mr-0'>
-        Hamburger
+      <div className='flex flex-col items-center sm:hidden mr-4'>
+        <div>
+          <div className='w-6 h-0.5 bg-gray-700 my-1'></div>
+          <div className='w-6 h-0.5 bg-gray-700 my-1'></div>
+          <div className='w-6 h-0.5 bg-gray-700 my-1'></div>
+        </div>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Typed from 'typed.js';
+import dp from '../assets/profile.jpg';
 
 function Home() {
   return (
-    <div>
-      Hi home
+    <div className='sm:grid sm:grid-cols-2'>
+      <div className='sm:text-6xl text flex flex-col sm:pl-12 justify-center text-4xl ml-8 h-72 pt-24 mb-16'>
+        <p className='sm:mb-4'>Hi There,</p>
+        <div className='flex mb-4'>
+          <p className='sm:mr-4 mr-2'>I'm Hrishabh</p>
+          <p className='text-orange-600'>Patel</p>
+        </div>
+        <div className='flex mb-4'>
+        <p className='sm:text-3xl font-thin text-2xl mr-2'>I am a </p>
+        <p className='sm:text-3xl font-thin text-2xl text-red-700'>Web developer </p>
+        </div>
+        <button className='text-sm sm:w-32 bg-blue-800 text-white px-3 py-1 rounded-md w-36'>Download Resume</button>
+      </div>
+      <div className='flex items-center sm:justify-center sm:h-[70vh] sm:ml-24 justify-center'>
+        <div className='rounded-[50%] sm:h-72 sm:w-72 border-2 overflow-hidden h-56 w-56'>
+          <img src={dp}/></div>
+      </div>
     </div>
   )
 }
